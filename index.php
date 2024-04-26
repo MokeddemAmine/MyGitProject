edacb3cd6035d67248997dfb533b78229b401d12
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-04-26 17:42:00 +0200

    add a finished project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main page</title>
+</head>
+<body>
+    <h1>main page</h1>
+    <h2>heading h2</h2>
+    <h2>heading h2</h2>
+    <h2>heading h2</h2>
+    <h2>heading h2</h2>
+    <h3>f3</h3>
+</body>
+</html>
